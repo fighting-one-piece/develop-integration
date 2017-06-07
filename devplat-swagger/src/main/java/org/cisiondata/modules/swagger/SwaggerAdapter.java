6f6ab4576c18d76310493c715ebb1a2a189b5796
wulin <125906088@qq.com>
2017-06-07 19:29:59 +0800

    devplat swagger project code commit

---
+++ devplat-swagger/src/main/java/org/cisiondata/modules/swagger/SwaggerAdapter.java
@@ -0,0 +1,21 @@
+package org.cisiondata.modules.swagger;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Component
+@EnableWebMvc
+public class SwaggerAdapter extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	    registry.addResourceHandler("swagger-ui.html")
+	      .addResourceLocations("classpath:/META-INF/resources/");
+	 
+	    registry.addResourceHandler("/webjars/**")
+	      .addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+	
+}

e5e306d5c00410002a5150fe29c49ccca6cf7414
wulin <125906088@qq.com>
2017-07-07 21:30:41 +0800

    mongodb project code modify commit

--- devplat-mongo/src/main/java/org/cisiondata/utils/mongodb/MongoDBUtils.java
+++ devplat-mongo/src/main/java/org/cisiondata/utils/mongodb/MongoDBUtils.java
@@ -1,13 +1,22 @@
 package org.cisiondata.utils.mongodb;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
 
 public class MongoDBUtils {
+	
+	private Logger LOG = LoggerFactory.getLogger(MongoDBUtils.class);
 	
 	private MongoClient mongoClient = null;
     
@@ -23,19 +32,43 @@ public class MongoDBUtils {
         return MongoDBUtilsHolder.INSTANCE;  
     } 
     
-    private void initMongoClient() {  
-    	List<ServerAddress> serverAddressList = new ArrayList<ServerAddress>();  
-        ServerAddress serverAddress01 = new ServerAddress("172.20.100.20", 27018);  
-        ServerAddress serverAddress02 = new ServerAddress("172.20.100.115", 27018);  
-        serverAddressList.add(serverAddress01);  
-        serverAddressList.add(serverAddress02);  
-        List<MongoCredential> credentials = new ArrayList<MongoCredential>();  
-        MongoCredential credential = MongoCredential.createCredential(  
-                "root", "admin", "123456".toCharArray());  
-        credentials.add(credential);  
-        mongoClient = new MongoClient(serverAddressList, credentials);  
-    }  
-      
+    private void initMongoClient() {
+		Properties properties = new Properties();
+		InputStream in = null;
+		try {
+			in = MongoDBUtils.class.getClassLoader().getResourceAsStream("mongo/env.local.properties");
+			properties.load(in);
+			String clusterNodesTxt = properties.getProperty("mongo.cluster.nodes");
+			if (StringUtils.isBlank(clusterNodesTxt)) throw new RuntimeException("server addresses is not null");
+			String[] clusterNodes = clusterNodesTxt.indexOf(",") == -1 ? 
+					new String[]{clusterNodesTxt} : clusterNodesTxt.split(",");
+			List<ServerAddress> serverAddressList = new ArrayList<ServerAddress>(); 
+			for (int i = 0, len = clusterNodes.length; i < len; i++) {
+				String clusterNode = clusterNodes[i];
+				if (clusterNode.indexOf(":") == -1) continue;
+				String[] ipAndPort = clusterNode.split(":");
+				serverAddressList.add(new ServerAddress(ipAndPort[0], Integer.parseInt(ipAndPort[1]))); 
+				LOG.info("Mongo Cluster Host: {} : {}", ipAndPort[0], Integer.parseInt(ipAndPort[1]));
+			}
+			String username = properties.getProperty("mongo.cluster.username");
+			String password = properties.getProperty("mongo.cluster.password");
+			String database = properties.getProperty("mongo.cluster.database");
+			List<MongoCredential> credentials = new ArrayList<MongoCredential>();  
+	        MongoCredential credential = MongoCredential.createCredential(  
+	                username, database, password.toCharArray());  
+	        credentials.add(credential);  
+	        mongoClient = new MongoClient(serverAddressList, credentials);  
+		} catch (Exception e) {
+			LOG.error(e.getMessage(), e);
+		} finally {
+			try {
+				if (null != in) in.close();
+			} catch (IOException e) {
+				LOG.error(e.getMessage(), e);
+			}
+		}
+	}
+    
     public MongoClient getClient() {  
         return mongoClient;  
     }  

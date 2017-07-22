9361e369715d57b058d8aebb475f8a41385a6e49
wulin <125906088@qq.com>
2017-07-22 17:55:47 +0800

    add dynamic data source function

---
+++ devplat-spring/src/main/java/org/cisiondata/utils/ds/DynamicRoutingDataSource.java
@@ -0,0 +1,24 @@
+package org.cisiondata.utils.ds;
+
+import java.util.Map;
+
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+
+public class DynamicRoutingDataSource extends AbstractRoutingDataSource {
+
+	@Override
+	public void setTargetDataSources(Map<Object, Object> targetDataSources) {
+		super.setTargetDataSources(targetDataSources);
+	}
+	
+	@Override
+	public void setDefaultTargetDataSource(Object defaultTargetDataSource) {
+		super.setDefaultTargetDataSource(defaultTargetDataSource);
+	}
+	
+	@Override
+	protected Object determineCurrentLookupKey() {
+		return DataSourceContextHolder.getDataSource();
+	}
+
+}

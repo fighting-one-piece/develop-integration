cde0b6972e4b3f0505d85842ffebab9d753cacd8
wulin <125906088@qq.com>
2017-07-29 19:12:38 +0800

    dynamic data source code modify commit

--- devplat-spring/src/main/java/org/cisiondata/utils/ds/DynamicDataSourceAspect.java
+++ devplat-spring/src/main/java/org/cisiondata/utils/ds/DynamicDataSourceAspect.java
@@ -2,27 +2,48 @@ package org.cisiondata.utils.ds;
 
 import java.lang.reflect.Method;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 
-@Aspect
-@Component
+//@Aspect
+//@Order(0)  
+//@Component
 public class DynamicDataSourceAspect {
 
 	public static final Logger LOG = LoggerFactory.getLogger(DynamicDataSourceAspect.class);
+	
+	private static final String EXECUTION = "execution(* org.cisiondata.modules.*.service.*.*.*(..))";
 
-	@Around("execution(* org.cisiondata.modules.*.service.impl.*.*(..))")
+	@Before(EXECUTION)  
+    public void before(JoinPoint jp) throws Throwable {  
+		MethodSignature methodSignature = (MethodSignature) jp.getSignature();
+        Method targetMethod = methodSignature.getMethod();
+        if(targetMethod.isAnnotationPresent(TargetDataSource.class)){
+            String targetDataSource = targetMethod.getAnnotation(TargetDataSource.class).value();
+            System.err.println("target data source: " + targetDataSource);
+            DataSourceContextHolder.setDataSource(targetDataSource);
+        }
+    }  
+	
+	@AfterReturning(EXECUTION)  
+    public void afterReturning() throws Throwable {  
+        DataSourceContextHolder.clearDataSource();  
+    }  
+	
+	@Around(EXECUTION)
 	public Object proceed(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
 		try {
 			MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
 	        Method targetMethod = methodSignature.getMethod();
 	        if(targetMethod.isAnnotationPresent(TargetDataSource.class)){
 	            String targetDataSource = targetMethod.getAnnotation(TargetDataSource.class).value();
+	            System.err.println("target data source: " + targetDataSource);
 	            DataSourceContextHolder.setDataSource(targetDataSource);
 	        }
 			return proceedingJoinPoint.proceed();

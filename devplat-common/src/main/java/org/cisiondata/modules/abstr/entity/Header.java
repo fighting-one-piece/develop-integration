0f74e7b5e89e491ee50307df85f8e47bbac8b3c8
wulin <125906088@qq.com>
2017-09-20 15:37:12 +0800

    common header code modify commit

--- devplat-common/src/main/java/org/cisiondata/modules/abstr/entity/Header.java
+++ devplat-common/src/main/java/org/cisiondata/modules/abstr/entity/Header.java
@@ -15,6 +15,13 @@ public class Header implements Serializable {
 	/** 子域*/
 	private List<Header> children = null;
 	
+	public Header() {}
+	
+	public Header(String fieldEN, String fieldCH) {
+		this.fieldEN = fieldEN;
+		this.fieldCH = fieldCH;
+	}
+
 	public String getFieldEN() {
 		return fieldEN;
 	}
@@ -31,17 +38,37 @@ public class Header implements Serializable {
 		this.fieldCH = fieldCH;
 	}
 	
-	public void addChild(Header header) {
-		if (null == children) children = new ArrayList<Header>();
-		children.add(header);
-	}
-	
 	public List<Header> getChildren() {
 		return children;
 	}
 	
 	public void setChildren(List<Header> children) {
 		this.children = children;
+	}
+	
+	public void addChild(String fieldEN, String fieldCH) {
+		if (null == children) children = new ArrayList<Header>();
+		children.add(new Header(fieldEN, fieldCH));
+	}
+	
+	public void addChild(Header... headers) {
+		if (null == headers || headers.length == 0) {
+			throw new RuntimeException("headers error!");
+		}
+		if (null == children) children = new ArrayList<Header>();
+		for (int i = 0, len = headers.length; i < len; i++) {
+			children.add(headers[i]);
+		}
+	}
+	
+	public void addChild(String... fields) {
+		if (null == fields || fields.length == 0 || fields.length % 2 != 0) {
+			throw new RuntimeException("fields error!");
+		}
+		if (null == children) children = new ArrayList<Header>();
+		for (int i = 0, len = fields.length; i < len; i++) {
+			children.add(new Header(fields[i], fields[++i]));
+		}
 	}
 	
 }

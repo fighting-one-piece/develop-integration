05a55a0ddfcdae059e98bf76a5b407a440245725
wulin <125906088@qq.com>
2017-10-11 15:28:02 +0800

    remove devplat original project code

--- devplat/src/main/java/org/cisiondata/modules/auth/service/IShiroFilterChainService.java
+++
@@ -1,9 +0,0 @@
-package org.cisiondata.modules.auth.service;
-
-import org.cisiondata.utils.exception.BusinessException;
-
-public interface IShiroFilterChainService {
-
-	public void initFilterChains() throws BusinessException;
-	
-}
--- devplat/src/main/resources/crawler/mobile_crawler.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /home/ym/project/mobile_crawler
-pwd
-scrapy crawl mobile_spider -a mobile_number=$1
